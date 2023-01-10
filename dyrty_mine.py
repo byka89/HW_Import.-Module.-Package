63d33e51ff0379cad274f43c62a16eee8e8994e6
Alex Yablokov <ya.alex.89@gmail.com>
2023-01-10 18:22:29 +0300

    first commit

---
+++ dyrty_mine.py
@@ -0,0 +1,6 @@
+
+from application.people import *
+from application.salary import *
+
+calculate_salary()
+get_employees()
---
+++ main.py
@@ -0,0 +1,8 @@
+import datetime
+from application.salary import calculate_salary
+from application.people import get_employees
+
+if __name__ == '__main__':
+    get_employees()
+    calculate_salary()
+    print(datetime.date.today())
